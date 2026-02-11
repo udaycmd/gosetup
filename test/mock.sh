b609ff1f7e72f823487f8cee66bc47c8469aef1b
Uday Tiwari <udaytiwari720@gmail.com>
2026-02-11 16:39:03 +0530

    Fix: bug regarding interactive mode prompting user regardless of --force flag

--- test/mock.sh
+++ test/mock.sh
@@ -3,7 +3,11 @@
 mkdir -p "$HOME/.local"
 ./gosetup remove
 ./gosetup install 1.21.0
-export PATH="$HOME/.local/go/bin:$PATH"
+
+GOBIN="$HOME/.local/bin"
+echo "$GOBIN" >> $GITHUB_PATH
+export PATH="$GOBIN:$PATH"
+
 ./gosetup upgrade -f
 ./gosetup remove
 ./gosetup
