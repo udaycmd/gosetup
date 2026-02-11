ac46f87fdc30f330770e731f0c8a3386e2cf469b
Uday Tiwari <udaytiwari720@gmail.com>
2026-02-11 13:29:46 +0530

    final fix!

--- test/mock.sh
+++ test/mock.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-set -x
-./gosetup.sh remove
-echo $?
+go version
 mkdir -p "$HOME"/.local/ # create if not present
-
 ./gosetup.sh install 1.18 # installs at $HOME/.local/go
-./gosetup.sh install 1.18 # fails with exit code 1
-echo $?
+# shellcheck source=/dev/null
+source "$HOME"/.bashrc
+./gosetup.sh install 1.21.0
+echo "$GOROOT"
+# shellcheck source=/dev/null
+source "$HOME"/.bashrc
+go version
 ./gosetup.sh upgrade -f
-./gosetup.sh remove # removes the current installation
+./gosetup.sh remove
 echo $?
 ./gosetup.sh # prints message help into the console
