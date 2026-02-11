12ece423cb34c1edcebaa5bd95862fba9c0dbab3
Uday Tiwari <udaytiwari720@gmail.com>
2026-02-11 16:29:36 +0530

    resolve the github runner path issue

--- test/mock.sh
+++ test/mock.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p "$HOME/.local"
 ./gosetup remove
-./gosetup install 1.21.0 .
-echo "$GOROOT"/bin >> $GITHUB_PATH
+./gosetup install 1.21.0
+export PATH="$HOME/.local/go/bin:$PATH"
 ./gosetup upgrade -f
-echo "$GOROOT"/bin >> $GITHUB_PATH
 ./gosetup remove
 ./gosetup
