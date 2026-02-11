6d23f4b8834c096165aa6df24e6f3ac5d119618b
Uday Tiwari <udaytiwari720@gmail.com>
2026-02-11 16:12:42 +0530

    Fix: critical bug regarding archive extraction in wrong directory

--- test/mock.sh
+++ test/mock.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-go version
-mkdir -p "$HOME"/.local/ # create if not present
-./gosetup.sh install 1.18 # installs at $HOME/.local/go
-# shellcheck source=/dev/null
+./gosetup remove
 source "$HOME"/.bashrc
-./gosetup.sh install 1.21.0
-echo "$GOROOT"
-# shellcheck source=/dev/null
+./gosetup install 1.21.0 .
 source "$HOME"/.bashrc
-go version
-./gosetup.sh upgrade -f
-./gosetup.sh remove
-echo $?
-./gosetup.sh # prints message help into the console
+./gosetup upgrade -f
+./gosetup remove
+./gosetup
